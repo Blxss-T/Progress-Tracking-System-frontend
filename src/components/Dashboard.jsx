cc06983dc2beb105a3740f70d3367813395f38ee
Bless <tetaangelbless014@gmail.com>
2025-09-28 21:36:26 +0200

    Initial commit

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -9,21 +9,21 @@ function Dashboard() {
             return <h1>Welcome {name}</h1>
         }} */}
         <div className='flex justify-between p-1 '>
-         <span>
            <h2 className='text-3xl font-medium'>Dashboard</h2>
-           <p>Plan,prioritize,and accompish with ease.</p>
-          
-         </span>
-            
-           
             <div className='flex gap-1 flex-wrap-reverse'>
             <button className="w-42  bg-gradient-to-br from-green-950 to-green-700 text-white  p-2 rounded-3xl">
                 Add Project
             </button>
             <button className='w-40 border-3 border-green-950 text-green-950 p-2 rounded-3xl'>Import</button>
             </div>
-            
         </div>
+        <p>Plan, prioritize, and accomplish with ease.</p>
+            <div className="flex flex-wrap">
+              <div className='border rounded-2xl bg-amber-800'></div>
+              <div className='border rounded-2xl bg-amber-800'></div>
+              <div className='border rounded-2xl bg-amber-800'></div>
+              <div className='border rounded-2xl bg-amber-800'></div>
+             </div>
     </div>
   )
 }
