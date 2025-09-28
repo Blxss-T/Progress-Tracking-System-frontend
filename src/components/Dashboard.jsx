da48ed7936ce4d4eed08fc0372fd366b6b495789
Bless <tetaangelbless014@gmail.com>
2025-09-28 20:23:14 +0200

    Initial commit

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -9,11 +9,16 @@ function Dashboard() {
             return <h1>Welcome {name}</h1>
         }} */}
         <div className='flex justify-between p-1 '>
-            <h2>Dashboard</h2>
+         <span>
+           <h2 className='text-3xl font-medium'>Dashboard</h2>
+           <p>Plan,prioritize,and accompish with ease.</p>
+          
+         </span>
+            
+           
             <div className='flex gap-1 flex-wrap-reverse'>
             <button className="w-42  bg-gradient-to-br from-green-950 to-green-700 text-white  p-2 rounded-3xl">
-                <FontAwesomeIcon 
-                icon={FaPlus} /> Add Project
+                Add Project
             </button>
             <button className='w-40 border-3 border-green-950 text-green-950 p-2 rounded-3xl'>Import</button>
             </div>
--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -27,16 +27,16 @@ function Sidebar() {
 
   return (
     <div className="md:w-60 h-full mx-3 rounded-2xl bg-gray-200 shadow-md p-4 fixed top-1.5">
-      <h1 className="text-4xl">Tranquil</h1>
+      <h1 className="text-4xl text-green-950 mb-10">Tranquil</h1>
       {/* Menu Section */}
       <div className="mb-6">
-        <h2 className="text-gray-700 font-semibold mb-5 ml=8">Menu</h2>
+        <h2 className="text-black font-semibold mb-3 text-md">MENU</h2>
         <ul className="space-y-2">
           {menuItems.map((item, index) => (
             <li key={index}>
               <Link
                 to={`/${item.name.toLowerCase()}`}
-                className="flex items-center gap-3 px-6 py-2 rounded hover:bg-gray-200"
+                className="flex items-center gap-2 px-6 py-2 rounded hover:bg-gray-200"
               >
                 <FontAwesomeIcon icon={item.icon} className="text-gray-600" />
                 <span className="text-gray-700 font-medium">{item.name}</span>
@@ -48,13 +48,13 @@ function Sidebar() {
 
       {/* General Section */}
       <div>
-        <h2 className="text-gray-700 font-semibold mb-5 ml-8 ">General</h2>
+        <h2 className="text-black font-semibold mb-3 text-md">GENERAL</h2>
         <ul className="space-y-2">
           {generalItems.map((item, index) => (
             <li key={index}>
               <Link
                 to={`/${item.name.toLowerCase()}`}
-                className="flex items-center gap-3 px-6 py-2 rounded hover:bg-gray-200"
+                className="flex items-center gap-2 px-6 py-2 rounded hover:bg-gray-200"
               >
                 <FontAwesomeIcon icon={item.icon} className="text-gray-600" />
                 <span className="text-gray-700 font-medium">{item.name}</span>
