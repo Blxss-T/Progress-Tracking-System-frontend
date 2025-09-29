cbdf68a487c84c0f0a3ab9f7b13a76005fce70a7
Bless <tetaangelbless014@gmail.com>
2025-09-29 15:31:16 +0200

    Initial commit

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,27 +1,40 @@
 import React from 'react'
 import UserGreeting from './userGreeting'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFolder } from "@fortawesome/free-solid-svg-icons";
+
 
 function Dashboard() {
   return (
-    <div className='ml-64 h-screen rounded-2xl bg-gray-100 p-4 mt-1.5 mr-1'>
+    <div className='ml-64 min-h-screen rounded-2xl bg-gray-100 p-4 mt-1.5 mr-1'>
         {/* {function userGreet (name){
             return <h1>Welcome {name}</h1>
         }} */}
-        <div className='flex justify-between p-1 '>
+        <div className='flex flex-wrap justify-between p-1 '>
            <h2 className='text-3xl font-medium'>Dashboard</h2>
-            <div className='flex gap-1 flex-wrap-reverse'>
+            <div className='flex gap-1 flex-wrap'>
             <button className="w-42  bg-gradient-to-br from-green-950 to-green-700 text-white  p-2 rounded-3xl">
                 Add Project
             </button>
-            <button className='w-40 border-3 border-green-950 text-green-950 p-2 rounded-3xl'>Import</button>
+            <button className='w-40 border-3 border-green-950 text-green-950 p-2 rounded-3xl hover:bg-emerald-100'>Import</button>
             </div>
         </div>
         <p>Plan, prioritize, and accomplish with ease.</p>
          <div className="p-6 bg-gray-100 min-h-screen">
       <div className="grid grid-cols-4 gap-3">
-        {/* Top row - Stats Cards */}
-        <div className="bg-white rounded-2xl shadow p-4">Total Projects</div>
+       
+       <div className="bg-white rounded-2xl shadow p-4 flex flex-col items-center ">
+
+         <div className='flex flex-2 justify-center items-center gap-4'>
+           <p className="text-black ">Total Projects</p>
+           <div className="bg-green-50  p-2 rounded-full hover:shadow-green-50">
+            <FontAwesomeIcon icon={faFolder} size="md" />
+          </div>
+          </div>
+          <div>
+            <h2 className="text-4xl text-gray-800">24</h2>
+          </div>
+        </div>
         <div className="bg-white rounded-2xl shadow p-4">Ended Projects</div>
         <div className="bg-white rounded-2xl shadow p-4">Running Projects</div>
         <div className="bg-white rounded-2xl shadow p-4">Pending Projects</div>
