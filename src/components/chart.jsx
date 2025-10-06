5de05788df399db3a372bd3e21f45e8a1ef1caa9
Bless <tetaangelbless014@gmail.com>
2025-10-06 13:46:02 +0200

    Initial commit

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -2,10 +2,19 @@
 import React from 'react'
 
 function chart() {
+  const data =[
+    {name:"Monday", contributions:790},
+    {name:"Tuesday", contributions:790},
+    {name:"Wednesday", contributions:790},
+    {name:"Thursday", contributions:790}, 
+    {name:"Friday", contributions:790}, 
+    {name:"Saturday", contributions:790}, 
+    {name:"Sunday", contributions:790}
+  ]
   return (
     <div>
-      <BarChart >
-        
+      <BarChart  width>
+
       </BarChart>
     </div>
   )
