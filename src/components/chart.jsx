409bd94499fa85a50c074681c014ea058f08a4e0
Bless <tetaangelbless014@gmail.com>
2025-10-09 21:32:52 +0200

    Initial commit

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -2,16 +2,15 @@
 import React from 'react'
 import { Tooltip, XAxis, BarChart,Bar,YAxis,ResponsiveContainer } from 'recharts'
 
-const weekDays=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 function Chart() {
   const data =[
-    {contributions:790},
-    { contributions:790},
-    {contributions:790},
-    { contributions:890}, 
-    { contributions:90}, 
-    {contributions:90}, 
-    { contributions:790}
+    {name:"Monday",contributions:790},
+    {name:"Tuesday", contributions:790},
+    {name:"Wednesday",contributions:790},
+    {name:"Thursday", contributions:890}, 
+    {name:"Friday", contributions:90}, 
+    {name:"Saturday",contributions:90}, 
+    {name:"Sunday", contributions:790}
   ]
   return (
     <div>
